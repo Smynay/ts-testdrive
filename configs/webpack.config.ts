a2e351ac7dbe8ea61658645a2826796dc8241019
Nikita Schastnyy <smynay@yandex.ru>
2021-12-05 17:39:18 +0300

    add webpack plugins

--- configs/webpack.config.ts
+++ configs/webpack.config.ts
@@ -1,14 +1,32 @@
 import {resolve} from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
     context: resolve(__dirname, '../src'),
     mode: 'development',
     entry: './index.ts',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[fullhash].js',
         path: resolve(__dirname, '../dist')
     },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: '../public/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: resolve(__dirname, '../public/favicon.ico'),
+                    to: resolve(__dirname, '../dist'),
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[fullhash].css'
+        })
+    ]
 }
